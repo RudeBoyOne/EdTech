e4e46c32d030fc43a6895e40441bb2d0a1da9e95
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2022-10-21 21:35:59 -0300

    implementação da camadas model, com seus devidos mapeamentos JPA

--- api/edtech/src/main/java/com/edtech_api/domain/model/Extensao.java
+++ api/edtech/src/main/java/com/edtech_api/domain/model/Extensao.java
@@ -1,6 +1,14 @@
 package com.edtech_api.domain.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.NonNull;
@@ -10,14 +18,25 @@ import lombok.Setter;
 @Setter
 @Getter
 @RequiredArgsConstructor
+@Entity
 public class Extensao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idExtensao;
+	
 	@NonNull
 	private String nome;
+	
 	@NonNull
 	private String especialidade;
+	
 	@NonNull
 	private LocalDate dataInicio;
+	
 	@NonNull
 	private LocalDate dataTermino;
+	
+	@OneToMany(mappedBy = "extensao")
+	private List<Professor> professores = new ArrayList<>();
 }
